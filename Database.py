3be4db3b5ee50ad87be54b8cdfb816d83a204c0a
Madur <madur99@yahoo.com>
2021-06-11 20:25:37 +0800

    updated /join in Main.py

--- Database.py
+++ Database.py
@@ -3,6 +3,8 @@ import os
 
 MONGO_CONNECTION_URL = os.environ['MONGODBCONNECTION']
 client = MongoClient(MONGO_CONNECTION_URL)
+
+# For testing connection
 # db = client.test
 
 db = client.scheduler
