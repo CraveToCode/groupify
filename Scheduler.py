f8097f65e61bffae784e0bfe8cc2bfafee2feaa1
Madur <madur99@yahoo.com>
2021-06-11 21:31:43 +0800

    editted /participants in Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -102,7 +102,7 @@ def participants(update: Update, context: CallbackContext) -> int:
     mongo_participant_list = collection.find({'chat_id': chat_id})
     participant_list = []
     for participant in mongo_participant_list:
-        print(participant["user_id"])
+        print(participant["user_tele_id"])
         participant_list.append(participant["user_tele_id"])
     logger.info("Participant list: %s", participant_list)
     update.message.reply_text(
