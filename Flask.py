2230aa7b13fcd45b039f801ef8d5bc32dfba24a0
Bernard <bernardwan99@gmail.com>
2021-06-19 03:25:31 +0800

    Testing flask

--- Flask.py
+++ Flask.py
@@ -1,4 +1,10 @@
 import flask
+import Database
+
+# Database
+collection_users = Database.db.users
+collection_details = Database.db.user_details
+collection_meetups = Database.db.meetups
 
 mongobp = flask.Blueprint('mongobp', __name__)
 
@@ -6,7 +12,8 @@ mongobp = flask.Blueprint('mongobp', __name__)
 @mongobp.route('/<groupid>/<eventid>/<userid>/', methods=['GET', 'POST', 'PUT'])
 def getData(groupid, eventid, userid):
     # returns event name, participant list, timetable for that user
-    return f"{groupid}, {eventid}, {userid}"
+    #return f"{groupid}, {eventid}, {userid}"
+    return collection_meetups.find({"chat_id": groupid})
 
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from Main import app, PORT, TOKEN, updater
+from Main import app
 
 
 if __name__ == "__main__":
