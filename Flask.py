c6786ec3ae97ad0c62d775d279c1c000ed13ec60
Bernard <bernardwan99@gmail.com>
2021-06-19 03:11:25 +0800

    Testing flask

--- Flask.py
+++ Flask.py
@@ -6,7 +6,7 @@ mongobp = flask.Blueprint('mongobp', __name__)
 @mongobp.route('/<groupid>/<eventid>/<userid>/', methods=['GET', 'POST', 'PUT'])
 def getData(groupid, eventid, userid):
     # returns event name, participant list, timetable for that user
-    return "test"
+    return f"{groupid}, {eventid}, {userid}"
 
 
 
--- Main.py
+++ Main.py
@@ -139,20 +139,7 @@ dispatcher.add_handler(unknown_handler)
 # Start Flask app
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-#@app.route('/', methods=['GET'])
-#def home():
- #   return '''<h1>Distant Reading Archive</h1>
-#<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-@app.route('/<groupid>/<eventid>/<userid>/', methods=['GET', 'POST', 'PUT'])
-def getData(groupid, eventid, userid):
-    # returns event name, participant list, timetable for that user
-    return f"{groupid}, {eventid}, {userid}"
-
-#app.register_blueprint(mongobp)
-
-#app.run(port=PORT)
+app.register_blueprint(mongobp)
 
 # Start/Stop Bot
 if __name__ == "__main__":
