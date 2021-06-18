82621e11a8450f0c5f42802568743ff6c7eb529a
Bernard <bernardwan99@gmail.com>
2021-06-19 00:20:18 +0800

    Testing flask

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from Main import app
+from Main import app, PORT, TOKEN, updater
+
 
 if __name__ == "__main__":
     app.run()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,
+                          webhook_url="https://groupify-orbital.herokuapp.com/" + TOKEN)
+    updater.idle()
