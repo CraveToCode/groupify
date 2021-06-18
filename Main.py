dfaf32e40838fb9656a2429329517b5981e4db5a
Bernard <bernardwan99@gmail.com>
2021-06-18 23:13:52 +0800

    Modified home page

--- Main.py
+++ Main.py
@@ -145,6 +145,12 @@ updater.idle()
 # Start Flask app
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
 app.register_blueprint(mongobp)
 app.run()
 
