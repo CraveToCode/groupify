2102849f6646c610ac1472008af9c851390a3ad8
Bernard <bernardwan99@gmail.com>
2021-06-24 00:52:32 +0800

    Testing flask

--- FlaskConfig.py
+++ FlaskConfig.py
@@ -4,6 +4,7 @@ import flask
 import Database
 from flask import request, jsonify
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 # Database
 collection_users = Database.db.users
@@ -17,7 +18,7 @@ mongobp = flask.Blueprint('mongobp', __name__)
 def getData(groupid, eventid, userid):
     # returns event name, participant list, timetable for that user
     #return f"{groupid}, {eventid}, {userid}"
-    cursor = collection_meetups.find_one({"chat_id": int(groupid)})
+    cursor = collection_meetups.find_one({"chat_id": int(groupid), "_id": ObjectId(eventid)})
     print(cursor)
     response = jsonify(json.loads(dumps(cursor)))
     response.headers.add("Access-Control-Allow-Origin", "*")
